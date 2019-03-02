b4be4d1360e655062c3406ff9b6b8ff3e6afcbec
Adam Hahn <adam.hahn@gmail.com>
2019-03-02 09:34:03 -0500

    getting absolute diffs

--- wallbot-command.js
+++ wallbot-command.js
@@ -337,8 +337,8 @@ replServer.defineCommand('write',{
 						letterY += yDiff;
 
 						if (bottomLengths != null) {
-							var rDiff = plannedRightLength - bottomLengths.rightLength;
-							var lDiff = plannedLeftLength - bottomLengths.leftLength;
+							var rDiff = Math.abs(plannedRightLength - bottomLengths.rightLength);
+							var lDiff = Math.abs(plannedLeftLength - bottomLengths.leftLength);
 							console.log('rDiff ' + rDiff + ' lDiff ' + lDiff);
 							commandList.push('r ' + rDiff);
 							plannedRightLength += rDiff;
